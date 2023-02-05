06052aa358cffbc5d92ad2efe885dc6be24a8d15
yilizzz <yilizhang3@gmail.com>
2023-02-05 20:26:45 +0100

    update

--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/login.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/login.js
@@ -2,14 +2,11 @@ function ListenerLoginCheck() {
     const formLogin = document.querySelector("#logData");
     formLogin.addEventListener("submit", async function (event) {
         event.preventDefault();
-        // 
         const logData = {
             email: event.target.querySelector("[name=email]").value,
             password: event.target.querySelector("[name=password]").value
         };
-        // 
         const chargeUtile = JSON.stringify(logData);
-        // 
         const response = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
@@ -4,13 +4,13 @@ if(sessionStorage.getItem('token') === "none"  || sessionStorage.getItem('token'
 	document.querySelector(".btnModify").style.display="none";
 	document.querySelector("#logout").style.display="none";
 	document.querySelector("#login").style.display="block";
-    document.querySelector("#mode").style.display="none";
+    document.querySelector(".mode").classList.add("hidden");
     document.querySelector(".categories").style.display = "block";
 }else{
     document.querySelector(".btnModify").style.display="block";
 	document.querySelector("#logout").style.display="block";
 	document.querySelector("#login").style.display="none";
-    document.querySelector("#mode").style.display="block";
+    document.querySelector(".mode").classList.remove("hidden");
     document.querySelector(".categories").style.display = "none";
 
 }
@@ -28,13 +28,7 @@ linkLogout.addEventListener("click", function () {
 
 var reponse = await fetch('http://localhost:5678/api/works/');
 var allWorks = await reponse.json();
-       
-async function updateAllWorks(){
-    
-    let reponse = await fetch('http://localhost:5678/api/works/');
-    allWorks = await reponse.json();
-    
-}
+
 //////////////////////////////////////////////////generate the elements to show all the works
 function generateGallery(works) {
 
@@ -62,13 +56,12 @@ function generateGallery(works) {
 }
 
 generateGallery(allWorks);
-
-
 ////////////////////////////////////////////////////////button publier les changements
-const btnUpdate = document.querySelector("#btn_update");
-btnUpdate.addEventListener("click", function(){
-    updateAllWorks();
-    generateGallery(allWorks);
+const btnUpdate = document.getElementById("btn_update");
+
+btnUpdate.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.replace("index.html")
 });
 
 ////////////////////////////////////////////////////////////////gestion des filter bouttons 
@@ -121,8 +114,6 @@ btnHR.addEventListener("click", function () {
 });
 
 /////////////////////////////////////////////////////////generate the elements in window modify
-
-
 function generateTobeEdit(works) {
 	// Récupération de l'élément du DOM qui accueillera les photos
 	const divWorks = document.querySelector("#works");
@@ -130,9 +121,10 @@ function generateTobeEdit(works) {
     for (let i = 0; i < works.length; i++) {
 		const oneWork = document.createElement("div");
         const work = works[i];
+        oneWork.id="div"+work.id;
         // Création des balises 
         const imgWork = document.createElement("img");
-        imgWork.id = work.id;
+        //imgWork.id = work.id;
         imgWork.src = work.imageUrl;
         imgWork.alt = work.title ?? "(No Title)";
 		imgWork.setAttribute("crossorigin","anonymous");
@@ -178,24 +170,23 @@ delFunc.addEventListener("click", async function(event){
                 }
             });
             if(res.status==204){
-            alert("Une photo a été supprimée.");
+                alert("Une photo a été supprimée.");
+                const divDel = document.getElementById("div"+id);
+                document.getElementById("works").removeChild(divDel);
             }else{
-            alert("Échec de la suppression.");
+                alert("Échec de la suppression: "+res.status);
             }
-
         }
-        // updateAllWorks();
-        // generateGallery(allWorks);
-        // generateTobeEdit(allWorks);
     }
 });
 //////////////////////////show or hide the window modify with a layer who covers other elements 
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
-var openModalBtn = document.querySelector(".btnModify");
+var openModalBtn = document.querySelector("#btnModal");
 var closeModalBtn = document.querySelector(".btnClose");
 var btnAdd = document.querySelector("#btnAdd");
-var sectionPortfolio = document.querySelector("#portfolio");
+var modalAdd = document.querySelector(".addWork");
+
 //function button "modifier"
 const openModal = function () {
 	modal.classList.remove("hidden");
@@ -212,21 +203,17 @@ closeModalBtn.addEventListener("click", closeModal);
 //overlay clicked: close window modify or window adding photo
 const closeWindows = function(){
     closeModal();
-    if(document.getElementById("addWork") != null){
-        sectionPortfolio.removeChild(document.getElementById("addWork"));
-    }
+    modalAdd.classList.add("hidden");
 }
 overlay.addEventListener("click", closeWindows);
 
 ///////////////////////////////////////////////////////////////////delete all
-
 const delAll= document.querySelector("#btnDeleteAll");
 delAll.addEventListener("click",async function(){
     
     let res = confirm("Êtes-vous sûr de supprimer tous?");
     if(res==true){
         allWorks.forEach(async (element)=>{
-           
             const res = await fetch("http://localhost:5678/api/works/"+parseInt(element.id),{
                 method:"DELETE",
                 headers:{
@@ -234,13 +221,10 @@ delAll.addEventListener("click",async function(){
                 }
             })
         });
+        document.getElementById("works").innerHTML="";
     }     
 });
-
-
 //////////////////////////////////////fonction of button "Ajouter une photo", hide modify window, generate a window for adding a work
-
-
 /////////////////////////////////////////////////////////Récupération des categories 
 var ctg = window.localStorage.getItem('ctg');
 var inputCount;
@@ -266,10 +250,8 @@ function listCategories(ctg){
 		// Création des balises 
 		const opt = document.createElement("option");
         const category = ctg[i];
-        
         opt.innerText = category.name;
 		opt.value = category.id;
-
         // On rattache la balise au div
 		divCtg.appendChild(opt);
 	}
@@ -278,145 +260,56 @@ function listCategories(ctg){
 btnAdd.addEventListener("click", function(){
     //hide window for modify
 	modal.classList.add("hidden");
-    //generate parent div
-    const addModel = document.createElement("div");
-    addModel.id="addWork";
-    sectionPortfolio.appendChild(addModel);
-    //generate a div for button X and button <---
-    const operation = document.createElement("div");
-    operation.id="operation";
-
-    const iconReturn = document.createElement("i");
-    iconReturn.classList.add("fa-solid");
-    iconReturn.classList.add("fa-arrow-left");
-    iconReturn.classList.add("fa-2x");
-    iconReturn.id = "btnEsc";
-
-    const iconClose = document.createElement("i");
-    iconClose.classList.add("fa-sharp");
-    iconClose.classList.add("fa-solid");
-    iconClose.classList.add("fa-xmark");
-    iconClose.classList.add("fa-2x");
-    iconClose.id = "btnAddClose";
-
-    addModel.appendChild(operation);
-    operation.appendChild(iconReturn);
-    operation.appendChild(iconClose);
-    //generate a form for update a photo
-    const form_work = document.createElement("form");
-    form_work.id = "form_work";
-    form_work.enctype="multipart/form-data";
-
-    const h2Title = document.createElement("h2");
-    h2Title.innerText = "Ajout photo";
-
-    addModel.appendChild(form_work);
-    form_work.appendChild(h2Title);
-    //generate a div for input
-    const upload_div = document.createElement("div");
-    upload_div.id = "upload_div";
-
-    const icon_img = document.createElement("i");
-    icon_img.classList.add("fa-solid");
-    icon_img.classList.add("fa-image");
-    icon_img.classList.add("fa-4x");
-
-    const btnUpload = document.createElement("a");
-    btnUpload.href = "#";
-    btnUpload.innerText = "+ Ajouter photo";
-    btnUpload.id="btn_upload";
-    
-
-    const upload_input = document.createElement("input");
-    upload_input.id = "upload_input";
-    upload_input.type = "file";
-    upload_input.name = "file";
-    upload_input.accept = ".jpg, .png";
-    btnUpload.appendChild(upload_input);
-    
-    
-    
-    const tip_p = document.createElement("p");
-    tip_p.innerText = "jpg, png : 4mo max";
-
-    //generate a img hidden for laterly showing the uploaded photo
-    const show = document.createElement("img");
-    show.id = "show";
-    show.src = "";
-    show.classList.add("hidden");
-
-    upload_div.appendChild(icon_img);
-    upload_div.appendChild(btnUpload);
-    upload_div.appendChild(tip_p);
-    upload_div.appendChild(show);
-    form_work.appendChild(upload_div);
-    
-    //generate an input for photo's title and a select for photo'category
-    const label1 = document.createElement("label");
-    label1.innerText = "Titre";
-    const inputTitle = document.createElement("input");
-    inputTitle.id="title";
-    inputTitle.name = "title";
-    inputTitle.type = "text";
-
-    const label2 = document.createElement("label");
-    label2.innerText = "Catégorie";
-    const optionCtg = document.createElement("select");
-    optionCtg.id = "optionCtg";
-    optionCtg.name = "optionCtg";
-
-    const line = document.createElement("div");
-    line.id="line_valid";
-
-    const btnSend = document.createElement("input");
-    btnSend.id="send";
-    btnSend.type = "submit";
-    btnSend.value = "Valider";
-    line.appendChild(btnSend);
-
-    form_work.appendChild(label1);
-    form_work.appendChild(inputTitle);
-    form_work.appendChild(label2);
-    form_work.appendChild(optionCtg);
-    form_work.appendChild(line);
-   
+    //show window for adding photo
+    modalAdd.classList.remove("hidden");
+    //clear all form's inputs
 	document.querySelector("#optionCtg").innerHTML = "";
 	listCategories(ctg);
+    document.querySelector("#title").value="";
+    document.querySelector("#upload_input").value="";
+    document.querySelector("#show").src="";
+    document.getElementById('upload_div').classList.remove("hidden");
     //initiate form's input number for validation
     inputCount=0;
     document.getElementById('send').disabled = true;
-
+    document.getElementById('send').style.backgroundColor="#A7A7A7";
 });
 
-
-
 ////////////////////////////////////////function button valider
-sectionPortfolio.addEventListener("submit", async function (event){
+document.getElementById('form_work').addEventListener("submit", async function (event){
     event.preventDefault();
-    // Création de l’objet du nouvel work.
     const formData = new FormData();
-    formData.append("image", event.target.querySelector("[name=file]").files[0]);
-    formData.append("title", event.target.querySelector("[name=title]").value);
-    formData.append("category", parseInt(event.target.querySelector("[name=optionCtg]").value));
-    
-    // Appel de la fonction fetch avec toutes les informations nécessaires
+    formData.append("image", document.querySelector("[name=file]").files[0]);
+    formData.append("title", document.querySelector("[name=title]").value);
+    formData.append("category", parseInt(document.querySelector("[name=optionCtg]").value));
     const response = await fetch("http://localhost:5678/api/works", {
-        method: "POST",
-        //headers: { "Content-Type": "application/json" },
-        headers: {'Authorization': 'Bearer ' + sessionStorage.getItem("token")},
-        body: formData
-    });
+          method: "post",
+          headers: {'Authorization': 'Bearer ' + sessionStorage.getItem("token")},
+          body: formData
+       })
     if(response.status==201){
         alert("Votre photo a été téléchargée.");
+        //clear form for next upload
+        document.querySelector("#title").value="";
+        document.querySelector("#upload_input").value="";
+        document.getElementById('upload_div').classList.remove("hidden");
+        document.querySelector("#show").src="";
+        document.querySelector("#show").classList.add("hidden");
+        document.querySelector("#optionCtg").innerHTML = "";
+	    listCategories(ctg);
+        
+        inputCount=0;
+        document.getElementById('send').style.backgroundColor="#A7A7A7";
+        document.getElementById('send').disabled = true;
+        
     }else{
         alert("Le téléchargement de la photo a échoué: "+response.status);
     }
-    
-})
+ })
+
 //get photo'url
 function getObjectURL(file) {
 	let url = null ;
-
 	if (window.createObjectURL!=undefined) { // basic
 		url = window.createObjectURL(file) ;
 	} else if (window.URL!=undefined) { // mozilla(firefox)
@@ -427,9 +320,7 @@ function getObjectURL(file) {
 	return url ;
 }
  ////////////////////////////valid check for the new photo's data: url, title and category
-
-sectionPortfolio.addEventListener("change", function(event){
-   
+ modalAdd.addEventListener("change", function(event){
     if((event.target.id=="upload_input")&&(event.target.value!="")){
         //////////////////////////////////////show this photo in the input zone
         var newSrc=getObjectURL(document.getElementById('upload_input').files[0]);
@@ -439,11 +330,8 @@ sectionPortfolio.addEventListener("change", function(event){
         }else{
             document.getElementById('show').classList.remove("hidden");
             document.getElementById('show').src=newSrc;
-            
             document.getElementById('upload_div').classList.add("hidden");
-            document.getElementById('upload_input').classList.add("hidden");
         }
-
         inputCount++;
     }
     if((event.target.value!=="")&&(event.target.id=="title")){
@@ -455,25 +343,28 @@ sectionPortfolio.addEventListener("change", function(event){
     if(inputCount>2){
         document.getElementById('send').disabled = false;
         document.getElementById('send').style.backgroundColor="#1D6154";
-        document.getElementById('send').style.color = "white";
     }
  
 })
 
 
 ////////////////////////////////////////function button X and button <---
-sectionPortfolio.addEventListener("click", function(event){
-    //event.stopPropagation();
+modalAdd.addEventListener("click", function(event){
+    
     let whichButton = event.target.id;
-    switch(whichButton){
-        case 'btnAddClose':
+    if( (whichButton=="btnAddClose") || (whichButton=="btnEsc") )
+    {
+        document.getElementById('show').src = "";
+        document.getElementById("show").classList.add("hidden");
+        document.getElementById('upload_div').classList.remove("hidden");
+        
+        modalAdd.classList.add("hidden");
+        if(whichButton=="btnAddClose"){
             overlay.classList.add("hidden");
-            sectionPortfolio.removeChild(document.getElementById("addWork"));
-            break;
-        case 'btnEsc':
-            sectionPortfolio.removeChild(document.getElementById("addWork"));
+        }
+        if(whichButton=="btnEsc"){
             modal.classList.remove("hidden");
-            break;
+        }
     }
       
 })
